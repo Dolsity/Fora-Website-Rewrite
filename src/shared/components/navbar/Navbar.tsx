433c1bcc7c9649583fd4106dd902277412c94b65
Jay <Acorder43812@gmail.com>
2023-05-15 01:03:13 -0700

    Update Navbar.tsx

--- src/shared/components/navbar/Navbar.tsx
+++ src/shared/components/navbar/Navbar.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './Navbar.module.scss';
+import Logo from '../../../assets/logo.png';
+
 
 export default function Navbar() {
   return (
@@ -7,15 +9,12 @@ export default function Navbar() {
       <div className={styles.navbar_container}>
         
         <a href="#" className={styles.navbar_logo}>
-          <img src="./" alt="Logo" />
+          <img src={Logo} alt="Logo" />
           Fora
         </a>
 
         <ul className={styles.navbar_menu}>
           <li className={styles.navbar_item}>
-            <a href="#" className={styles.navbar_links}>Home</a> 
-            <a href="#" className={styles.navbar_links}>Dashboard</a>  
-            <a href="#" className={styles.navbar_links}>Commands</a>
             <a href="#" className={styles.navbar_links}>Login</a>
           </li>
         </ul>
