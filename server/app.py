891bddabfcffffe10e24f84dbddec8f84e99ce5f
Jay <Acorder43812@gmail.com>
2023-05-18 01:12:33 -0700

    Update app.py

--- server/app.py
+++ server/app.py
@@ -5,10 +5,11 @@ from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+
 CORS(app)
 
 load_dotenv()
-os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "true" # !! Only in development environment.
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "true" # !! Only in development environment
 app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
 app.config["DISCORD_CLIENT_ID"] = os.getenv('DISCORD_CLIENT_ID')
 app.config["DISCORD_CLIENT_SECRET"] = os.getenv('DISCORD_CLIENT_SECRET')
@@ -51,9 +52,14 @@ def home():
 
 @app.route('/api/data/')
 def api_data():
+    if not discord.authorized:
+        return jsonify({"error": "Unauthorized"}), 401
+    
+    user = discord.fetch_user()
 
     data = {
         'message': 'Hello from Flask API!',
+        'username': user.name,
     }
     return jsonify(data)
 
